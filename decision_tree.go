37837ed81e9d5542158e6914242967b19e9e4805
dyv <dvisher1@gmail.com>
2015-03-28 20:03:54 -0400

    added ActOnResponse(patient, msg), as well as decisiontree do

--- decision_tree.go
+++ decision_tree.go
@@ -4,6 +4,19 @@ package main
 
 // each level of a decision tree has a
 type DecisionTree struct {
+	Server   Server
 	Action   Action
 	Decision map[YesNoIdk]*DecisionTree
+}
+
+func (d *DecisionTree) Do(s *Server, resp string) *DecisionTree {
+	yni := IsYesNoIdk(resp)
+	dnext := d.Decision[yni]
+	if dnext == nil {
+		return dnext
+	}
+	msg := dnext.Action.String()
+	p := dnext.Action.Patient()
+	s.SendSMS(p, msg)
+	return dnext
 }
--- server.go
+++ server.go
@@ -13,3 +13,8 @@ type Server struct {
 func NewServer(pn string) *Server {
 	return &Server{PhoneNumber: pn, Actions: make([]Action, 0)}
 }
+
+func (s *Server) ActOnResponse(p *Patient, resp string) {
+	dt := p.DecisionTree
+	p.DecisionTree = dt.Do(s, resp)
+}
